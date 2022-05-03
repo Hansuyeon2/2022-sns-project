19ae89099290dd74c9f397dbb12247157a12c715
Hansuyeon2 <hansuyeon23@naver.com>
2022-05-03 21:01:58 +0900

    week3

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def showmain(request):
+    return render(request, 'main/mainpage.html')
+def showintroduce(request):
     return render(request, 'main/show.html')
-
 
