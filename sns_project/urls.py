fbd1024e07f513dbf52167878527a1a49030a94e
Hansuyeon2 <hansuyeon23@naver.com>
2022-05-10 20:16:57 +0900

    week4

--- sns_project/urls.py
+++ sns_project/urls.py
@@ -5,5 +5,8 @@ from main import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.showmain, name="showmain"),
-    path('show/', views.showintroduce, name="showintroduce")
+    path('show/', views.showintroduce, name="showintroduce"),
+    path('<int:id>',views.detail, name="detail"),
+    path('new/',views.new, name="new"),
+    path('create/',views.create, name="create"),
 ]
